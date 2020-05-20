b378e4ddc4334b5837d0b39aee39d6494bea8f39
Kyle Tilley <katilley10@gmail.com>
2020-05-20 12:02:32 -0600

    changed default line ending, filled in basic schema layout, added index.js to server folder

--- database/ProductSchema.js
+++ database/ProductSchema.js
@@ -1,1 +1,12 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Greenfield Reviews');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => console.log('connection established to Greenfield Reviews'));
+
+const reviewSchema = new mongoose.Schema({});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
