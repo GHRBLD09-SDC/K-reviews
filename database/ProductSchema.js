afe6736cd654fda7e9b88bd251f8e3925569679c
Kyle Tilley <katilley10@gmail.com>
2020-05-20 12:28:22 -0600

    researching indexes and my schema

--- database/ProductSchema.js
+++ database/ProductSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Greenfield Reviews');
+mongoose.connect('mongodb://localhost/Greenfield Reviews', { autoIndex: false });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', () => console.log('connection established to Greenfield Reviews'));
