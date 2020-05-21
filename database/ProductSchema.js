33f95a4e7c6415cbf6a688470f96f80da8e548cd
Kyle Tilley <katilley10@gmail.com>
2020-05-20 20:25:43 -0600

    built schema

--- database/ProductSchema.js
+++ database/ProductSchema.js
@@ -5,7 +5,40 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', () => console.log('connection established to Greenfield Reviews'));
 
-const reviewSchema = new mongoose.Schema({});
+const reviewSchema = new mongoose.Schema({
+  product_id: String,
+  page: Number,
+  count: Number,
+  results: [{
+    review_id: Number,
+    rating: Number,
+    summary: String,
+    recommend: Number,
+    response: String,
+    body: String,
+    date: String,
+    reviewer_name: String,
+    helpfullness: Number,
+    photos: [{
+      id: Number,
+      url: String,
+    }],
+  }],
+  characteristics: {
+    size: {
+      id: Number,
+      value: String,
+    },
+    width: {
+      id: Number,
+      value: String,
+    },
+    confort: {
+      id: Number,
+      value: String,
+    },
+  },
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
