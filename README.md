8499c3d09b58c0e205dc7f190d364849672623f3
Kyle Tilley <katilley10@gmail.com>
2020-05-20 19:42:20 -0600

    tested server routes for connection and updated readme to include what each route will do

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ This is the repo for the 'reviews' module. The 'reviews' module will be found in
 >## Scripts to run
 > #### npm run server
 > - runs the server by running node on index.js in the server file
+
+
+>## CRUD Routes
+>#### List Reviews
+> - Returns a list of reviews for a particular product. This list *does not* include any reported reviews
+> - > GET /reviews/:product_id/list
+>
+>#### Get Review Metadata
+> - Returns review metadata for a given product
+> - > GET /reviews/:product/id/meta
+>
+>#### Add a Review
+> - Adds a review for the given product
+> - > POST /reviews/:product_id
+>
+>#### Mark Review as Helpful
+> - Updates a review to show it was found helpful
+> - > PUT /reviews/helpful/:review_id
+>
+>#### Report Review
+> - Updates a review to show it was reported. Note, this action does not delete the review, but the review will not be returned in the above GET request
+> - > PUT /reviews/report/:review_id
--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
 const express = require('express');
+const bp = require('body-parser');
 
 const app = express();
 const port = 8153;
 
 app.listen(port, () => console.log(`Listenting at http://localhost:${port}`));
+app.use(bp.json());
 
 
 //Returns a list of reviews for a particular product. This list does not include any reported reviews.
-app.get('/revies/:product_id/list', (req, res) => {
-
+app.get('/reviews/:product_id/list', (req, res) => {
+  res.send('test1');
 });
 
 //Returns review metadata for a given product
 app.get('/reviews/:product_id/meta', (req, res) => {
-
+  res.send('test2');
 });
 
 //Adds a review for the given product
-app.post('.reviews/:product_id', (req, res) => {
-
+app.post('/reviews/:product_id', (req, res) => {
+  res.send('test3');
 });
 
 //Updates a review to show it was found helpful
 app.put('/reviews/helpful/:review_id', (req, res) => {
-
+  res.send('test4');
 });
 
 //Updates a review to show it was reported. Note, this action does not delete the review, but the review will not be returned in the above GET request.
 app.put('/reviews/report/:review_id', (req, res) => {
-
+  res.send('test5');
 });
