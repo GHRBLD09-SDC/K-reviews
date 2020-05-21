d3732b3904104a51e5296336921b6013a1f26ccc
Kyle Tilley <katilley10@gmail.com>
2020-05-21 12:28:44 -0600

    found out how to used a tracker to track how long it takes to seed my db

--- database/db-functions/insertProducts.js
+++ database/db-functions/insertProducts.js
@@ -2,52 +2,56 @@ const faker = require('faker');
 const { PerformanceObserver, performance } = require('perf_hooks');
 const Product = require('../ProductSchema.js');
 
-const obs = new PerformanceObserver((items) => { //don't know if I need lines 5-11
-  console.log(items.getEntries()[0].duration); //the "undefined" performance mark has not been set
-  performance.clearMarks();
-});
-obs.observe({ entryTypes: ['measure'] });
-performance.measure('Start to Now');
+const seeder = () => {
+  for (let i = 0; i < 100000; i += 1) {
+    const product = new Product({
+      product_id: '1',
+      page: 0,
+      count: 5,
+      results: [{ //randomize how many results 5-10
+        review_id: 1,
+        rating: Math.floor(Math.random() * 5 + 1),
+        summary: faker.fake('{{lorem.sentence}}'),
+        recommend: 0,
+        response: '',
+        body: faker.fake('{{lorem.sentence}}'),
+        date: 'today',
+        reviewer_name: faker.fake('{{name.firstName}} {{name.lastName}}'),
+        helpfullness: Math.floor(Math.random() * 5 + 1),
+        photos: [{ //randomize how many photos
+          id: 1,
+          url: faker.fake('{{image.image}}'),
+        }],
+        characteristics: {
+          size: {
+            id: 1,
+            value: Math.floor(Math.random() * 5 + 1),
+          },
+          width: {
+            id: 1,
+            value: Math.floor(Math.random() * 5 + 1),
+          },
+          confort: {
+            id: 1,
+            value: Math.floor(Math.random() * 5 + 1),
+          },
+          quality: {
+            id: 1,
+            value: Math.floor(Math.random() * 5 + 1),
+          },
+        },
+      }],
+    });
+    product.save();
+    if (i % 1000 === 0) console.log(i);
+  }
+};
 
-performance.mark('A');
-const product = new Product({
-  product_id: '1',
-  page: 1,
-  count: 5,
-  results: [{ //randomize how many results 5-10
-    review_id: 1,
-    rating: Math.floor(Math.random() * 5 + 1),
-    summary: faker.fake('{{lorem.sentence}}'),
-    recommend: 0,
-    response: '',
-    body: faker.fake('{{lorem.sentence}}'),
-    date: 'today',
-    reviewer_name: faker.fake('{{name.firstName}} {{name.lastName}}'),
-    helpfullness: Math.floor(Math.random() * 5 + 1),
-    photos: [{ //randomize how many photos
-      id: 1,
-      url: faker.fake('{{image.image}}'),
-    }],
-    characteristics: {
-      size: {
-        id: 1,
-        value: Math.floor(Math.random() * 5 + 1),
-      },
-      width: {
-        id: 1,
-        value: Math.floor(Math.random() * 5 + 1),
-      },
-      confort: {
-        id: 1,
-        value: Math.floor(Math.random() * 5 + 1),
-      },
-      quality: {
-        id: 1,
-        value: Math.floor(Math.random() * 5 + 1),
-      },
-    },
-  }],
-});
+const wrapped = performance.timerify(seeder);
 
-product.save();
-performance.measure('A to Now', 'A');
+const obs = new PerformanceObserver((list) => {
+  console.log(list.getEntries()[0].duration);
+  obs.disconnect();
+});
+obs.observe({ entryTypes: ['function'] });
+wrapped();
