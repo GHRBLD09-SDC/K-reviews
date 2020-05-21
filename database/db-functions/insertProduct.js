38e256b095a50b238294dd949f1de0e0754ef2a0
Kyle Tilley <katilley10@gmail.com>
2020-05-20 21:30:34 -0600

    test ride of db ready

--- database/db-functions/insertProduct.js
+++ database/db-functions/insertProduct.js
@@ -1,2 +1,43 @@
+const faker = require('faker');
 const Product = require('../ProductSchema.js');
 
+const product = new Product({
+  product_id: '1',
+  page: 1,
+  count: 5,
+  results: [{ //randomize how many results 5-10
+    review_id: 1,
+    rating: Math.floor(Math.random() * 5 + 1),
+    summary: faker.fake('{{lorem.sentence}}'),
+    recommend: 0,
+    response: '',
+    body: faker.fake('{{lorem.sentence}}'),
+    date: 'today',
+    reviewer_name: faker.fake('{{lorem.firstName}} {{lorem.lastName}}'),
+    helpfullness: Math.floor(Math.random() * 5 + 1),
+    photos: [{ //randomize how many photos
+      id: 1,
+      url: faker.fake('{{image.image}}'),
+    }],
+    characteristics: {
+      size: {
+        id: 1,
+        value: Math.floor(Math.random() * 5 + 1),
+      },
+      width: {
+        id: 1,
+        value: Math.floor(Math.random() * 5 + 1),
+      },
+      confort: {
+        id: 1,
+        value: Math.floor(Math.random() * 5 + 1),
+      },
+      quality: {
+        id: 1,
+        value: Math.floor(Math.random() * 5 + 1),
+      },
+    },
+  }],
+});
+
+product.svae();
