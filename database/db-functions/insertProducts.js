f7dccc3e2ae57bbca9ec775e09cb527dc5aa6168
Kyle Tilley <katilley10@gmail.com>
2020-05-22 12:43:28 -0600

    fixed data generator!

--- database/db-functions/insertProducts.js
+++ database/db-functions/insertProducts.js
@@ -3,22 +3,22 @@ const fs = require('fs');
 const { PerformanceObserver, performance } = require('perf_hooks');
 
 const seeder = () => {
-  for (let i = 0; i < 1000000; i += 1) {
+  for (let i = 0; i < 10000000; i += 1) {
     const review = {
-      review_id: 1,
-      product_id: i,
-      rating: Math.floor(Math.random() * 5 + 1),
+      review_id: '1',
+      // product_id: i,
+      // rating: Math.floor(Math.random() * 5 + 1),
       summary: faker.fake('{{lorem.sentence}}'),
-      recommend: 0,
+      recommend: '0',
       response: '',
       body: faker.fake('{{lorem.sentence}}'),
       date: 'today',
       reviewer_name: faker.fake('{{name.firstName}} {{name.lastName}}'),
-      helpfullness: Math.floor(Math.random() * 5 + 1),
-      photos: [{ //randomize how many photos
-        id: 1,
-        url: faker.fake('{{image.image}}'),
-      }],
+      // helpfullness: Math.floor(Math.random() * 5 + 1),
+      // photos: [{ //randomize how many photos
+        // id: 1,
+        // url: faker.fake('{{image.image}}'),
+      // }],
       characteristics: [1, 2, 5],
     };
     fs.writeFileSync('database/data.json', JSON.stringify(review), { flag: 'as' });
