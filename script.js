75467c8e5d928c68ad1397ee239f70fa731b82bf
Kyle Tilley <katilley10@gmail.com>
2020-05-28 20:15:52 -0600

    break from testing how k6 works. Having problems checking my status codes through it

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
-export let options = {
-  vus: 20,
-  duration: '30s',
+export const options = {
+  vus: 30,
+  duration: '5s',
 };
 
+const localHelpful = 'http://localhost:8154/reviews/helpful/1';
+const ec2Helpful = 'http://ec2-3-133-97-46.us-east-2.compute.amazonaws.com:8153/reviews/helpful/1';
+
 export default () => {
-  http.get('http://ec2-3-133-97-46.us-east-2.compute.amazonaws.com:8153/reviews/helpful/1');
+  const res = http.get(localHelpful);
+  check(res, {
+    'is status 204': (r) => r.status === 204,
+  });
   sleep(1);
-}
+};
