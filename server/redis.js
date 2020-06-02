02c3549126715d4abeafac8becbecc88b299b3b9
Kyle Tilley <katilley10@gmail.com>
2020-06-01 20:09:04 -0600

    trying to use redis in the wrong query, come back to it tomorrow and try it with the get all

--- server/redis.js
+++ server/redis.js
@@ -1,13 +1,16 @@
 const redis = require('redis');
 
-const report = require('./controllers.js');
+const { report } = require('./controllers.js');
 
 const client = redis.createClient({ host: '127.0.0.1', port: 6379 });
 
 client.on('error', (err) => console.log(err));
 
 exports.redisGet = (req, res) => {
-  client.get(req.params.product_id, (err, res) => {
-    console.log('here');
+  client.get(req.params.product_id, (err) => {
+    if (err) {
+      client.set(req.params.product_id)
+      report(req, res);
+    }
   });
 };
